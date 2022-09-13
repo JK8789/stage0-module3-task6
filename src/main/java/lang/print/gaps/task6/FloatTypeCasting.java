77602f0f42c03679daf0a719ca72b02039a23367
Korotkova Julia <korotkovayuliav@gmail.com>
2022-09-13 16:12:54 +0200

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,12 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int result = (int)Math.round(numberToBeRounded);
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        FloatTypeCasting floatTypeCasting = new FloatTypeCasting();
+        floatTypeCasting.roundNumber(3.5f);
     }
 }
